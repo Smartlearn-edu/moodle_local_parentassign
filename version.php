f91d2b2864464bfd8749f5f5011f5dc381d2cb4c
Smat Learn <113486938+Smartlearn-edu@users.noreply.github.com>
2026-03-10 03:52:54 +0200

    Update plugin version to 2023112400

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_parentassign';
-$plugin->version   = 2023112300;
+$plugin->version   = 2023112400;
 $plugin->requires  = 2022112800; // Moodle 4.1+.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '1.0.0';
